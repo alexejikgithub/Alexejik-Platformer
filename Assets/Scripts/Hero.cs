7920f049340c16a83e05880062dd4450486d1128
alexejik <alexeysmirnov1992@gmail.com>
2021-03-09 23:11:01 +0300

    Added horizontal and vertical movement

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Platformer
+{
+    public class Hero : MonoBehaviour
+    {
+
+        [SerializeField] private float _speed;
+
+        private Vector3 _direction;
+
+        public void SetDirection(Vector3 direction)
+        {
+            _direction = direction;
+        }
+
+        private void Update()
+        {
+            if (_direction != Vector3.zero)
+            {
+                var deltaX = _direction.x * _speed * Time.deltaTime;
+                var newXposition = transform.position.x + deltaX;
+                var deltaY = _direction.y * _speed * Time.deltaTime;
+                var newYposition = transform.position.y + deltaY;
+
+                transform.position = new Vector3(newXposition, newYposition, transform.position.z);
+            }
+        }
+        public void SaySomehting()
+        {
+            Debug.Log("Somehing");
+        }
+
+
+
+
+
+    }
+
+}
---
+++ Assets/UserInput/HeroInputAction.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/UserInput/HeroInputAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @HeroInputAction : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @HeroInputAction()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""HeroInputAction"",
+    ""maps"": [
+        {
+            ""name"": ""Hero"",
+            ""id"": ""3cf3f46a-017d-4055-a35e-a33602611bc2"",
+            ""actions"": [
+                {
+                    ""name"": ""HorizontalMovement"",
+                    ""type"": ""Button"",
+                    ""id"": ""ceb47c7c-1f14-4026-b642-9945b2a73e2a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""SaySomething"",
+                    ""type"": ""Button"",
+                    ""id"": ""bcfacc00-62d7-4ee3-9b0a-3a3a31ffdddf"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector keyboard"",
+                    ""id"": ""d2b57bc6-cf01-422d-ad45-deceb08afe57"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""HorizontalMovement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""63f22c77-97ce-47b5-add7-e40abbefa9de"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""HorizontalMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""514ad3cd-079c-4b9c-842a-772a30d51ca0"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""HorizontalMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""96bdeaae-9c7b-4203-8f9b-791affe6ca7c"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""HorizontalMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""243a2e29-cfc8-4c65-a1cc-3bbded8ff611"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""HorizontalMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3216600d-6ad1-4cbe-9c24-775aa474c8a2"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SaySomething"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Hero
+        m_Hero = asset.FindActionMap("Hero", throwIfNotFound: true);
+        m_Hero_HorizontalMovement = m_Hero.FindAction("HorizontalMovement", throwIfNotFound: true);
+        m_Hero_SaySomething = m_Hero.FindAction("SaySomething", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Hero
+    private readonly InputActionMap m_Hero;
+    private IHeroActions m_HeroActionsCallbackInterface;
+    private readonly InputAction m_Hero_HorizontalMovement;
+    private readonly InputAction m_Hero_SaySomething;
+    public struct HeroActions
+    {
+        private @HeroInputAction m_Wrapper;
+        public HeroActions(@HeroInputAction wrapper) { m_Wrapper = wrapper; }
+        public InputAction @HorizontalMovement => m_Wrapper.m_Hero_HorizontalMovement;
+        public InputAction @SaySomething => m_Wrapper.m_Hero_SaySomething;
+        public InputActionMap Get() { return m_Wrapper.m_Hero; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(HeroActions set) { return set.Get(); }
+        public void SetCallbacks(IHeroActions instance)
+        {
+            if (m_Wrapper.m_HeroActionsCallbackInterface != null)
+            {
+                @HorizontalMovement.started -= m_Wrapper.m_HeroActionsCallbackInterface.OnHorizontalMovement;
+                @HorizontalMovement.performed -= m_Wrapper.m_HeroActionsCallbackInterface.OnHorizontalMovement;
+                @HorizontalMovement.canceled -= m_Wrapper.m_HeroActionsCallbackInterface.OnHorizontalMovement;
+                @SaySomething.started -= m_Wrapper.m_HeroActionsCallbackInterface.OnSaySomething;
+                @SaySomething.performed -= m_Wrapper.m_HeroActionsCallbackInterface.OnSaySomething;
+                @SaySomething.canceled -= m_Wrapper.m_HeroActionsCallbackInterface.OnSaySomething;
+            }
+            m_Wrapper.m_HeroActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @HorizontalMovement.started += instance.OnHorizontalMovement;
+                @HorizontalMovement.performed += instance.OnHorizontalMovement;
+                @HorizontalMovement.canceled += instance.OnHorizontalMovement;
+                @SaySomething.started += instance.OnSaySomething;
+                @SaySomething.performed += instance.OnSaySomething;
+                @SaySomething.canceled += instance.OnSaySomething;
+            }
+        }
+    }
+    public HeroActions @Hero => new HeroActions(this);
+    public interface IHeroActions
+    {
+        void OnHorizontalMovement(InputAction.CallbackContext context);
+        void OnSaySomething(InputAction.CallbackContext context);
+    }
+}
