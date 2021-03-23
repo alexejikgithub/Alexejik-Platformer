c17c094a6784d0f3d1a27e48931c3eedac926304
alexejik <alexeysmirnov1992@gmail.com>
2021-03-23 21:21:06 +0300

    Added serializable class StatesOfAnimation

--- Assets/Scripts/SpriteAnimation/SpriteAnimation.cs
+++ Assets/Scripts/SpriteAnimation/SpriteAnimation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -13,8 +14,9 @@ namespace Platformer
 		[SerializeField] private int _frameRate;
 		private Sprite[] _sprites; // no need to make [SerializeField] because _sprites come from _animationStates._Sprites
 		[SerializeField] private UnityEvent _onComplete;
-		[SerializeField] private MyAnimationStates[] _animationStates; // Need to choose objects in the inspector
-		private MyAnimationStates _currentAnimationState; // 
+		[SerializeField] private StatesOfAnimation[] _animationStates; // Need to choose objects in the inspector
+		private StatesOfAnimation _currentAnimationState; // 
+		private int _currentAnimationIndex;
 
 		private SpriteRenderer _renderer;
 		private float _secondsPerFrame;
@@ -45,6 +47,7 @@ namespace Platformer
 			if(_nameOfTheClip!=null)
 			{
 				SetClip(_nameOfTheClip); // change _currentAnimationState
+				_nameOfTheClip = null;
 			}
 			
 
@@ -62,6 +65,14 @@ namespace Platformer
 				{
 					_currentSpriteIndex = 0;
 				}
+				else if (_currentAnimationState._allowNext&& _currentAnimationIndex< _animationStates.Length-1) // If Allow next animation state and there are animation states left
+				{
+					_currentSpriteIndex = 0;
+					_currentAnimationIndex++;
+					_currentAnimationState = _animationStates[_currentAnimationIndex++];
+					
+
+				}
 
 				else
 				{
@@ -69,6 +80,7 @@ namespace Platformer
 
 					_onComplete?.Invoke();
 					enabled = false;
+					
 					return;
 
 				}
@@ -88,12 +100,21 @@ namespace Platformer
 				if (_animationStates[i]._name == name)
 				{
 					_currentAnimationState = _animationStates[i];
+					_currentAnimationIndex = i;
 				}
 			}
 		}
 
 
 
+	}
+	[Serializable]
+	public class StatesOfAnimation
+	{
+		[SerializeField] public string _name;
+		[SerializeField] public bool _loop;
+		[SerializeField] public bool _allowNext;
+		[SerializeField] public Sprite[] _sprites;
 	}
 }
 
