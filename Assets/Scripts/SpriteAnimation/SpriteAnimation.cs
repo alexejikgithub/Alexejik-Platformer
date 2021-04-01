eadcb79edb381b03d99c149d42375e5632886720
alexejik <alexeysmirnov1992@gmail.com>
2021-04-02 00:45:09 +0300

    Some Extra Changes to the scene and animation code

--- Assets/Scripts/SpriteAnimation/SpriteAnimation.cs
+++ Assets/Scripts/SpriteAnimation/SpriteAnimation.cs
@@ -13,7 +13,6 @@ namespace Platformer
 		[SerializeField] private string _nameOfTheClip; //Set name of the clip that you want to play
 		[SerializeField] private int _frameRate;
 		private Sprite[] _sprites; // no need to make [SerializeField] because _sprites come from _animationStates._Sprites
-		[SerializeField] private UnityEvent _onComplete;
 		[SerializeField] private StatesOfAnimation[] _animationStates; // Need to choose objects in the inspector
 		private StatesOfAnimation _currentAnimationState; // 
 		private int _currentAnimationIndex;
@@ -28,11 +27,13 @@ namespace Platformer
 		private void Start()
 		{
 			_renderer = GetComponent<SpriteRenderer>();
-			
-
-
 
 		}
+
+
+
+
+
 		private void OnEnable()
 		{
 			_secondsPerFrame = 1f / _frameRate;
@@ -44,15 +45,15 @@ namespace Platformer
 
 		private void Update()
 		{
-			if(_nameOfTheClip!=null)
+			if (_nameOfTheClip != null)
 			{
 				SetClip(_nameOfTheClip); // change _currentAnimationState
 				_nameOfTheClip = null;
 			}
-			
 
 
-			_sprites = _currentAnimationState._sprites; // if _currentAnimationState is changed, sprites also change
+
+			_sprites = _currentAnimationState.Sprites; // if _currentAnimationState is changed, sprites also change
 			if (_nextFrameTime > Time.time)
 			{
 				return;
@@ -61,16 +62,20 @@ namespace Platformer
 
 			if (_currentSpriteIndex >= _sprites.Length)
 			{
-				if (_currentAnimationState._loop)
+				if (_currentAnimationState.Loop)
 				{
 					_currentSpriteIndex = 0;
 				}
-				else if (_currentAnimationState._allowNext&& _currentAnimationIndex< _animationStates.Length-1) // If Allow next animation state and there are animation states left
+				else if (_currentAnimationState.AllowNext) // If Allow next animation state and there are animation states left
 				{
 					_currentSpriteIndex = 0;
-					_currentAnimationIndex++;
-					_currentAnimationState = _animationStates[_currentAnimationIndex++];
-					
+					if (_currentAnimationIndex == _animationStates.Length - 1)
+					{
+						_currentAnimationIndex = 0;
+					}
+					else _currentAnimationIndex++;
+					_currentAnimationState = _animationStates[_currentAnimationIndex];
+
 
 				}
 
@@ -78,9 +83,9 @@ namespace Platformer
 				{
 
 
-					_onComplete?.Invoke();
+					_currentAnimationState.OnComplete?.Invoke();
 					enabled = false;
-					
+
 					return;
 
 				}
@@ -90,14 +95,14 @@ namespace Platformer
 			_currentSpriteIndex++;
 
 
-			
+
 
 		}
 		public void SetClip(string name)
 		{
 			for (int i = 0; i < _animationStates.Length; i++)
 			{
-				if (_animationStates[i]._name == name)
+				if (_animationStates[i].Name == name)
 				{
 					_currentAnimationState = _animationStates[i];
 					_currentAnimationIndex = i;
@@ -111,10 +116,20 @@ namespace Platformer
 	[Serializable]
 	public class StatesOfAnimation
 	{
-		[SerializeField] public string _name;
-		[SerializeField] public bool _loop;
-		[SerializeField] public bool _allowNext;
-		[SerializeField] public Sprite[] _sprites;
+		[SerializeField] private string _name;
+		[SerializeField] private bool _loop;
+		[SerializeField] private bool _allowNext;
+		[SerializeField] private Sprite[] _sprites;
+		[SerializeField] private UnityEvent _onComplete;
+
+
+		public string Name => _name;
+		public bool Loop => _loop;
+		public bool AllowNext => _allowNext;
+		public Sprite[] Sprites => _sprites;
+		public UnityEvent OnComplete => _onComplete;
+
+
 	}
 }
 
