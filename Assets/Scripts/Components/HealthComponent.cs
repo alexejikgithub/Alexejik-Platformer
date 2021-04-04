c41bd076f1b70b573d8a14362c553192bb106f05
alexejik <alexeysmirnov1992@gmail.com>
2021-04-04 14:24:33 +0300

    Saw sprite + invincibility frames

--- Assets/Scripts/Components/HealthComponent.cs
+++ Assets/Scripts/Components/HealthComponent.cs
@@ -7,27 +7,46 @@ namespace Platformer.Components
 {
 	public class HealthComponent : MonoBehaviour
 	{
-		[Range (0,100)]
+		[Range(0, 100)]
 		[SerializeField] private int _health;
 		[SerializeField] private UnityEvent _onDamage;
 		[SerializeField] private UnityEvent _onHealing;
 		[SerializeField] private UnityEvent _onDie;
 
+		private bool _invincibilityFrames;
+		private void Awake()
+		{
+			_invincibilityFrames = false;
+		}
 		public void ApplyDamage(int damageValue)
 		{
-			_health -= damageValue;
-			_onDamage?.Invoke();
-			if(_health<=0)
+			if (!_invincibilityFrames)
 			{
-				_onDie?.Invoke();
+				_health -= damageValue;
+				_onDamage?.Invoke();
+				if (_health <= 0)
+				{
+					_onDie?.Invoke();
+				}
 			}
+
 		}
 
 		public void ApplyHealing(int healingValue)
 		{
 			_health += healingValue;
 			_onHealing?.Invoke();
-			
+
+		}
+
+		public void InvincibilityFramesTrue()
+		{
+			_invincibilityFrames = true;
+		}
+
+		public void InvincibilityFramesFalse()
+		{
+			_invincibilityFrames = false;
 		}
 
 	}
