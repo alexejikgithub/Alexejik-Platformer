23ef2f7206cd2e587e2a4225748cd44377f8bd1d
alexejik <alexeysmirnov1992@gmail.com>
2021-04-10 16:44:25 +0300

    The latest changes 10.04.2021

--- Assets/Scripts/NewSpriteAnimation.cs
+++ Assets/Scripts/NewSpriteAnimation.cs
@@ -10,124 +10,129 @@ namespace Platformer
 
 	public class NewSpriteAnimation : MonoBehaviour
 	{
-		[SerializeField] private string _nameOfTheClip; //Set name of the clip that you want to play
-		[SerializeField] private int _frameRate;
-		private Sprite[] _sprites; // no need to make [SerializeField] because _sprites come from _animationStates._Sprites
-		[SerializeField] private StatesOfAnimation[] _animationStates; // Need to choose objects in the inspector
-		private StatesOfAnimation _currentAnimationState; // 
-		private int _currentAnimationIndex;
+
+		[SerializeField] [Range(1,30)] private int _frameRate =10;
+		[SerializeField] private UnityEvent<string> _onComplete;
+		[SerializeField] private AnimationClip[] _clips; // Need to choose objects in the inspector
 
 		private SpriteRenderer _renderer;
-		private float _secondsPerFrame;
-		private int _currentSpriteIndex;
+
+		private float _secPerFrame;
 		private float _nextFrameTime;
+		private int _currentFrame;
+		private bool _isPlaying = true;
+
+		private int _currentClip;
+
+
 
 
 
 		private void Start()
 		{
 			_renderer = GetComponent<SpriteRenderer>();
+			_secPerFrame = 1f / _frameRate;
+
+			StartAnimation();
 
 		}
 
+		private void OnBecameVisible()
+		{
+			enabled = _isPlaying;
+		}
+
+		private void OnBecameInvisible()
+		{
+			enabled = false;
+		}
+		public void SetClip(string clipName)
+		{
 		
+			
+			for (int i = 0; i < _clips.Length; i++)
+			{
+				if (_clips[i].Name == clipName)
+				{
+					
+					_currentClip = i;
+					StartAnimation();
+					return;
+				}
+			}
+			enabled = _isPlaying = false;
+		}
+		private void StartAnimation()
+		{
+			_nextFrameTime = Time.time + _secPerFrame;
+			enabled = _isPlaying = true;
+			_currentFrame = 0;
+		}
 
 
 
 		private void OnEnable()
 		{
-			_secondsPerFrame = 1f / _frameRate;
-			_nextFrameTime = Time.time + _secondsPerFrame;
-			//_currentSpriteIndex = 0;
-			_currentAnimationState = _animationStates[0];
+			
+			_nextFrameTime = Time.time + _secPerFrame;
+			
 
 		}
 
 		private void Update()
 		{
-			if(_nameOfTheClip!=null)
-			{
-				SetClip(_nameOfTheClip); // change _currentAnimationState
-				_nameOfTheClip = null;
-			}
-			
+			if (_nextFrameTime > Time.time) return;
 
-
-			_sprites = _currentAnimationState.Sprites; // if _currentAnimationState is changed, sprites also change
-			if (_nextFrameTime > Time.time)
+			var clip = _clips[_currentClip];  
+			if (_currentFrame >= clip.Sprites.Length)
 			{
+				if(clip.Loop)
+				{
+					_currentFrame = 0;
+				}
+				else
+				{
+					enabled = _isPlaying = clip.AllowNextClip;
+					clip.OnComplete?.Invoke();
+					_onComplete?.Invoke(clip.Name);
+					if(clip.AllowNextClip)
+					{
+						_currentFrame = 0;
+						_currentClip = (int)Mathf.Repeat(_currentClip + 1, _clips.Length);
+					}
+				}
 				return;
 			}
 
-
-			if (_currentSpriteIndex >= _sprites.Length)
-			{
-				if (_currentAnimationState.Loop)
-				{
-					_currentSpriteIndex = 0;
-				}
-				else if (_currentAnimationState.AllowNext) // If Allow next animation state and there are animation states left
-				{
-					_currentSpriteIndex = 0;
-					if(_currentAnimationIndex == _animationStates.Length - 1)
-					{
-						_currentAnimationIndex = 0;
-					}
-					else _currentAnimationIndex++;
-					_currentAnimationState = _animationStates[_currentAnimationIndex];
-					
-
-				}
-
-				else
-				{
-
-					
-					_currentAnimationState.OnComplete?.Invoke();
-					//enabled = false;
-					
-					return;
-
-				}
-			}
-			_renderer.sprite = _sprites[_currentSpriteIndex];
-			_nextFrameTime += _secondsPerFrame;
-			_currentSpriteIndex++;
+		
 
 
 			
+			_renderer.sprite = clip.Sprites[_currentFrame];
+			_nextFrameTime += _secPerFrame;
+			_currentFrame++;
+						
 
 		}
-		public void SetClip(string name)
-		{
-			_currentSpriteIndex = 0;
-			for (int i = 0; i < _animationStates.Length; i++)
-			{
-				if (_animationStates[i].Name == name)
-				{
-					_currentAnimationState = _animationStates[i];
-					_currentAnimationIndex = i;
-				}
-			}
-		}
+		
 
 
 
 	}
 	[Serializable]
-	public class StatesOfAnimation
+	public class AnimationClip
 	{
 		[SerializeField] private string _name;
+		[SerializeField] private Sprite[] _sprites;
 		[SerializeField] private bool _loop;
 		[SerializeField] private bool _allowNext;
-		[SerializeField] private Sprite[] _sprites;
 		[SerializeField] private UnityEvent _onComplete;
 
 
 		public string Name => _name;
-		public bool Loop => _loop;
-		public bool AllowNext => _allowNext;
 		public Sprite[] Sprites => _sprites;
+		public bool Loop => _loop;
+		public bool AllowNextClip => _allowNext;
 		public UnityEvent OnComplete => _onComplete;
 
 
