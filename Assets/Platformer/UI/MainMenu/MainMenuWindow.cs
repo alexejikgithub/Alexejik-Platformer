4cd504c8e25e1e30062eef0e64770a50f528f542
alexejik <alexeysmirnov1992@gmail.com>
2021-05-15 00:38:33 +0300

    UI Lection

---
+++ Assets/Platformer/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Platformer.UI.MainMenu
+{
+	public class MainMenuWindow : AnimatedWindow
+
+	{
+
+		private Action _closeAction;
+
+		public void OnStartGame()
+		{
+			_closeAction = () => { SceneManager.LoadScene("Level1"); };
+			Close();
+		}
+		public void OnShowSettings()
+		{
+			var window = Resources.Load<GameObject>("UI/SettingsWindow");
+			var canvas = FindObjectOfType<Canvas>();
+			Instantiate(window, canvas.transform);
+		}
+		public void OnExit()
+		{
+			_closeAction = () =>
+			{
+				base.OnCloseAnimationComplete();
+
+				Application.Quit();
+
+#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+#endif
+			};
+			Close();
+		}
+
+		public override void OnCloseAnimationComplete()
+		{
+			_closeAction?.Invoke();
+			base.OnCloseAnimationComplete();
+
+			
+		}
+
+	}
+}
