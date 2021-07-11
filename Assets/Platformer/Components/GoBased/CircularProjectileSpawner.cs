d8d0fdae7ef5bd834fe1a88cb1c993a0bf1b69bb
alexejik <alexeysmirnov1992@gmail.com>
2021-07-11 22:12:30 +0300

    Boss1Lecture done

--- Assets/Platformer/Components/GoBased/CircularProjectileSpawner.cs
+++ Assets/Platformer/Components/GoBased/CircularProjectileSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using Platformer.Creatures.Weapons;
 using Platformer.Utils;
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -7,10 +8,9 @@ namespace Platformer.Components.GoBased
 {
 	public class CircularProjectileSpawner : MonoBehaviour
 	{
+		[SerializeField] private CircularProjectileSettings[] _settings;
 
-		[SerializeField] private DirectionalProjectile _projectile;
-		[SerializeField] private int _burstCount;
-		[SerializeField] private float _delay;
+		public int Stage { get; set; }
 
 
 		[ContextMenu("Launch")]
@@ -23,19 +23,32 @@ namespace Platformer.Components.GoBased
 
 		private IEnumerator SpawnProjectiles()
 		{
-			var sectorStep = 2 * Mathf.PI / _burstCount;
+			var settings = _settings[Stage];
+			var sectorStep = 2 * Mathf.PI / settings.BurstCount;
 			
-			for (int i = 0; i < _burstCount; i++)
+			for (int i = 0; i < settings.BurstCount; i++)
 			{
 				var angle = sectorStep * i;
 				
 				var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-				var instance = SpawnUtils.Spawn(_projectile.gameObject, transform.position);
+				var instance = SpawnUtils.Spawn(settings.Prefab.gameObject, transform.position);
 				var projectile = instance.GetComponent<DirectionalProjectile>();
 				projectile.Launch(direction);
 				
-				yield return new WaitForSeconds(_delay);
+				yield return new WaitForSeconds(settings.Delay);
 			}
 		}
+	}
+
+	[Serializable]
+	public struct CircularProjectileSettings
+	{
+		[SerializeField] private DirectionalProjectile _projectile;
+		[SerializeField] private int _burstCount;
+		[SerializeField] private float _delay;
+
+		public DirectionalProjectile Prefab => _projectile;
+		public int BurstCount => _burstCount;
+		public float Delay => _delay;
 	}
 }
---
+++ Assets/Platformer/Creatures/Mobs/Boss/ChangeLightsComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace Platformer.Creatures.Mobs.Boss
+{
+	public class ChangeLightsComponent : MonoBehaviour
+	{
+		[SerializeField] private Light2D[] _lights;
+
+		[ColorUsage(true, true)]
+		[SerializeField] private Color _color;
+
+
+		[ContextMenu("Setup")]
+		public void SetColor()
+		{
+			foreach( var light2D in _lights)
+			{
+				light2D.color = _color;
+			}
+		}
+	}
+
+}
