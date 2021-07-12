bf1b145d82f3f4eecf6ac80af83118251df48c73
alexejik <alexeysmirnov1992@gmail.com>
2021-07-13 00:08:52 +0300

    BossBattle 1 HW

---
+++ Assets/BossBombStage.cs
@@ -0,0 +1,13 @@
+﻿using Platformer.Components;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBombStage : StateMachineBehaviour
+{
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		var spawner = animator.GetComponentInChildren<ProbabilityDropComponent>();
+		spawner.CalculateDrop();
+	}
+}
--- Assets/Platformer/Components/ColliderBased/EnterTriggerComponent.cs
+++ Assets/Platformer/Components/ColliderBased/EnterTriggerComponent.cs
@@ -13,26 +13,41 @@ namespace Platformer.Components.ColliderBased
 		[SerializeField] private LayerMask _layer = ~0;
 		[SerializeField] private EnterEvent _enterAction;
 		[SerializeField] private EnterEvent _exitAction;
+		[SerializeField] private EnterEvent _stay2DAction;
 
 
 		private void OnTriggerEnter2D(Collider2D other)
 		{
-
+			
 			if (!other.gameObject.IsInLayer(_layer)) return;
-
+			
 			if (!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return;
-
+			
 			_enterAction?.Invoke(other.gameObject);
-
+			
 		}
 
 		private void OnTriggerExit2D(Collider2D other)
 		{
+			
 			if (!other.gameObject.IsInLayer(_layer)) return;
 
 			if (!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return;
 
 			_exitAction?.Invoke(other.gameObject);
+			
+		}
+
+		private void OnTriggerStay2D(Collider2D other)
+		{
+			
+			if (!other.gameObject.IsInLayer(_layer)) return;
+
+			if (!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return;
+
+			_stay2DAction?.Invoke(other.gameObject);
+
+			
 		}
 		[Serializable]
 		public class EnterEvent : UnityEvent<GameObject>
