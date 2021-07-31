49011ff137e37e09aa672b973f80c6ac11f0f686
alexejik <alexeysmirnov1992@gmail.com>
2021-07-31 03:11:44 +0300

    Added boulder

---
+++ Assets/Platformer/Particles/ParticleSystemStopAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Platformer.Particles
+{
+	public class ParticleSystemStopAction : MonoBehaviour
+	{
+		[SerializeField] UnityEvent _event;
+
+		private void OnParticleSystemStopped()
+		{
+			_event?.Invoke();
+		}
+	}
+}
